5dfc6762f5598492b6745d5b85cda299eee03fc6
Araceli <araceli292.nm@gmail.com>
2022-01-15 16:22:12 +0000

    Fin Paso 41

--- app/Http/Livewire/CategoryFilter.php
+++ app/Http/Livewire/CategoryFilter.php
@@ -12,13 +12,14 @@ class CategoryFilter extends Component
     use WithPagination;
 
     public $category, $subcategoria, $marca;
+    public $view = 'grid';
 
     public function render()
     {
         $productsQuery = Product::query()->whereHas('subcategory.category', function(Builder $query){
             $query->where('id', $this->category->id);
         });
-        
+
         if ($this->subcategoria) {
             $productsQuery = $productsQuery->whereHas('subcategory', function(Builder $query){
                 $query->where('name', $this->subcategoria);
