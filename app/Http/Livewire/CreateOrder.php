c1faf22a21f7f91b2ef03070556930fbed6f477a
Araceli <araceli292.nm@gmail.com>
2022-01-24 16:17:52 +0000

    Fin Paso 95

--- app/Http/Livewire/CreateOrder.php
+++ app/Http/Livewire/CreateOrder.php
@@ -3,12 +3,14 @@
 namespace App\Http\Livewire;
 
 use App\Models\Department;
+use App\Models\Order;
+use Gloudemans\Shoppingcart\Facades\Cart;
 use Livewire\Component;
 
 class CreateOrder extends Component
 {
     public $envio_type = 1;
-    public $contact, $phone;
+    public $contact, $phone, $shipping_cost;
     public $address, $reference;
     public $departments, $cities = [], $districts = [];
     public $department_id = '', $city_id = '', $district_id = '';
@@ -19,17 +21,9 @@ class CreateOrder extends Component
         'envio_type' => 'required'
     ];
 
-    public function create_order()
+    public function mount()
     {
-        $rules = $this->rules;
-        if ($this->envio_type == 2) {
-            $rules['department_id'] = 'required';
-            $rules['city_id'] = 'required';
-            $rules['district_id'] = 'required';
-            $rules['address'] = 'required';
-            $rules['reference'] = 'required';
-        }
-        $this->validate($rules);
+        $this->departments = Department::all();
     }
 
     public function updatedEnvioType($value)
@@ -41,9 +35,35 @@ class CreateOrder extends Component
         }
     }
 
-    public function mount()
+    public function create_order()
     {
-        $this->departments = Department::all();
+        $rules = $this->rules;
+
+        if ($this->envio_type == 2) {
+            $rules['department_id'] = 'required';
+            $rules['city_id'] = 'required';
+            $rules['district_id'] = 'required';
+            $rules['address'] = 'required';
+            $rules['reference'] = 'required';
+        }
+
+        $this->validate($rules);
+
+        $order = new Order();
+
+        $order->user_id = auth()->user()->id;
+        $order->contact = $this->contact;
+        $order->phone = $this->phone;
+        $order->envio_type = $this->envio_type;
+        $order->shipping_cost = 0;
+        $order->total = $this->shipping_cost + Cart::subtotal();
+        $order->content = Cart::content();
+
+        $order->save();
+
+        Cart::destroy();
+
+        return redirect()->route('orders.payment', $order);
     }
 
     public function render()
