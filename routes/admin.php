d607facf4f7fb13b891b408a521b446998a84feb
Araceli <araceli292.nm@gmail.com>
2022-02-03 11:28:36 +0000

    Fin Paso 123

---
+++ routes/admin.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Livewire\Admin\ShowProducts;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', ShowProducts::class)->name('admin.index');
+
+Route::get('products/{product}/edit', function () {})->name('admin.products.edit');
